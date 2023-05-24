f4259f1056a926dbe2f10e8e3fd65809db0c02b0
Camila <camilappadalino@gmail.com>
2023-05-24 16:22:41 -0300

    ajustes

--- 98316 - Camila Padalino.py
+++ 98316 - Camila Padalino.py
@@ -3,32 +3,30 @@ Nome: Camila do Prado Padalino
 RM: 98316
 """
 
-def calcular_delta(a,b,c):
+def calculo_delta(a,b,c):
     delta = b ** 2 - 4 * a * c
-    if delta < 0:
+    equacaoXv = -b / (2 * a)
+    equacaoYv = - delta / (4 * a)
+    print(f"O X do vértice é {equacaoXv: .2f} \n e o Y do vértice é {equacaoYv: .2f}.") 
+
+    if delta > 0:
+        x1 = (-b + delta) / (2 * a)
+        x2 = (-b - delta) / (2 * a)
+        soma_raizes = x1 + x2
+        produto_raizes = x1 * x2
+        print(f"A primeira raiz vale: {x1: .2f} e a segunda raiz vale: {x2: .2f}.")
+        print(f" A soma das raízes é: {soma_raizes}")
+        print(f"O produto das raízes é {produto_raizes}")
+    
+    elif delta < 0:
         print("Não há raízes, pois o delta é negativo.")
     
     elif delta == 0:
         x = -b / (2 * a)
         print(f'As raízes são iguais, valem: {x}')
 
-    else:
-        x1 = (-b + delta) / (2 * a)
-        x2 = (-b - delta) / (2 * a)
-        soma_raizes = x1 + x2
-        produto_raizes = x1 * x2
-        print(f"A primeira raiz vale: {x1: .2f} \n e a segunda raiz vale: {x2: .2f}.")
-        print(f" A soma das raízes é: {soma_raizes: .2f}")
-        print(f"O produto das raízes é {produto_raizes: .2f}")
-
-    equacaoXv = -b / (2 * a)
-    equacaoYv = - delta / (4 * a)
-    print(f"O X do vértice é {equacaoXv: .2f} \n e o Y do vértice é {equacaoYv: .2f}.")
-
-
-
-
-def grafico_funcao(a,b,c):
+    
+def grafico_funcao_segundo_grau(a,b,c):
     i = -100
     x = []
     y = []
@@ -44,6 +42,6 @@ def grafico_funcao(a,b,c):
 a = int(input("Insira um valor: "))
 b = int(input("Insira um outro valor: "))
 c = int(input("Insira um último valor: "))
-calcular_delta(a,b,c)
-grafico_funcao(a,b,c)
+calculo_delta(a,b,c)
+grafico_funcao_segundo_grau(a,b,c)
 
